6cd11b17c69d116871524aa612ad7e26ed7b422f
Gabriel Garcia <gabriel@gg.local>
2018-07-02 16:23:56 -0400

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # sochain
-Node JS helper library for Chain.so API
+Node JS helper library for Chain.so API created by [GAFO TECH](https://gafo.tech).
+
+## Introduction
+
+Excerpt from [SoChain website](https://chain.so/api#introduction):
+
+> SoChain's *fast* blockchain API is the easiest, most cost-effective way to build applications on Bitcoin, Litecoin, Dogecoin, Zcash, and Dash. We also offer Test Networks for developers to get started in a sandbox environment. Currency is just the first application of the Blockchain, and there's alot more to come. We're helping you make the future happen.
+
+## Usage
+
+```javascript
+
+var SoChain = require('sochain');
+
+var chain = new SoChain('BTC'); // see below for possible networks
+
+// Get network info
+chain.info().then(console.log);
+
+// Get prices
+chain.prices().then(console.log);
+
+// Get block data
+chain.block(530000).then(console.log);
+
+// Get transaction data
+chain.tx('e3c1e99d53e031fe37c6f5c07c4089929647dd086b285b0b422283f9751d5294').then(console.log);
+
+// Get address data
+chain.address('1Jf2gLDsr4U6JhivD2Lu9eiH9rNJu6Hyxg').then(console.log);
+
+// Broadcast signed transaction hex
+chain.broadcast(hex).then(console.log);
+
+```
+
+## Networks
+
+| Network 	| Symbol 	| Information                                   	|
+|---------	|--------	|-----------------------------------------------	|
+| Bitcoin 	| BTC    	| The main Bitcoin network. Currency has value. 	|
+| Dash     	| DASH    | The main Dash network. Currency has value. 	    |
+| Zcash    	| ZEC     | The main Zcash network. Currency has value.     |
+| Dogecoin  | DOGE    | The main Dogecoin network. Currency has value.  |
+| Litecoin  | LTC     | The main Litecoin network. Currency has value.  |
+| Bitcoin (Test net) 	| BTCTEST    	| The main Bitcoin network. Currency has value. 	|
+| Dash (Test net)     | DASHTEST    | The main Dash network. Currency has value. 	    |
+| Zcash (Test net)    | ZECTEST     | The main Zcash network. Currency has value.     |
+| Dogecoin (Test net) | DOGETEST    | The main Dogecoin network. Currency has value.  |
+| Litecoin (Test net) | LTCTEST     | The main Litecoin network. Currency has value.  |
---
+++ index.js
@@ -0,0 +1,121 @@
+
+const request = require('request');
+
+class SoChain {
+  constructor (network) {
+    this.network = network;
+  }
+
+  /**
+   * get - perform GET request
+   *
+   * @param  {String} url the URL to GET
+   * @return {Promise}    resolves with result data
+   */
+  get (url) {
+    return new Promise((resolve,reject) => {
+      var options = {
+        uri: url,
+        method: 'GET'
+      };
+      request(options, (err,res,body) => {
+        if (err) return reject(err);
+        if (res.statusCode == 200) {
+          resolve(JSON.parse(body).data);
+        } else {
+          reject(new Error(body));
+        }
+      })
+    });
+  }
+
+  /**
+   * post - description
+   *
+   * @param  {String} url  the URL to POST
+   * @param  {Object} data the JSON data to POST
+   * @return {Promise}     resolves with result data
+   */
+  post (url, data) {
+    return new Promise((resolve,reject) => {
+      var options = {
+        uri: url,
+        method: 'POST',
+        json: true,
+        data: data
+      };
+      request(options, (err,res,body) => {
+        if (err) return reject(err);
+        if (res.statusCode == 200) {
+          resolve(JSON.parse(body).data);
+        } else {
+          reject(new Error(body));
+        }
+      })
+    });
+  }
+
+
+  /**
+   * async info - get network info
+   *
+   * @return {Promise}  resolves with network info
+   */
+  async info () {
+    return await this.get(`https://chain.so/api/v2/get_info/${this.network}`);
+  }
+
+
+  /**
+   * async prices - get network prices
+   *
+   * @return {Promise}  resolves with network prices
+   */
+  async prices() {
+    return await this.get(`https://chain.so/api/v2/get_price/${this.network}`);
+  }
+
+
+  /**
+   * async block - get block data
+   *
+   * @param  {String} id block hash or number
+   * @return {Promise}   resolves with block data
+   */
+  async block(id) {
+    return await this.get(`https://chain.so/api/v2/block/${this.network}/${id}`);
+  }
+
+  /**
+   * async tx - get transaction data
+   *
+   * @param  {String} id transaction ID
+   * @return {Promise}   resolves with tx data
+   */
+  async tx(id) {
+    return await this.get(`https://chain.so/api/v2/get_tx/${this.network}/${id}`);
+  }
+
+  /**
+   * async address - get address data
+   *
+   * @param  {String} id the address to lookup
+   * @return {Promise}   resolves with address data
+   */
+  async address(id) {
+    return await this.get(`https://chain.so/api/v2/address/${this.network}/${id}`);
+  }
+
+  /**
+   * async broadcast - broadcast signed transaction hex
+   *
+   * @param  {String} hex the signed hex string to send
+   * @return {Promise}    resolves with broadcast result
+   */
+  async broadcast(hex) {
+    return await this.post(`https://chain.so/api/v2/send_tx/${this.network}`, {tx_hex: hex});
+  }
+
+}
+
+module.exports = SoChain;
